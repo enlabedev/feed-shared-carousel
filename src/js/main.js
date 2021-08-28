bdeb2372a43d17c1f89424406428d4accf45c087
Enrique Lazo Bello <enlabe@gmail.com>
2021-08-28 14:05:05 -0500

    feat: Integración del slider con las imágenes.

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,9 @@
 
  const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
+  slidesPerView: "auto",
+  spaceBetween: 8,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
   },
 });
