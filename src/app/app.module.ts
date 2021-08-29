d81008c71244d541b51fa0c7b1cda04ee60606ca
Enrique Lazo Bello <enlabe@gmail.com>
2021-08-29 06:53:57 -0500

    feat: Se creó la estructura base.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddUsersComponent } from './components/add-users/add-users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddUsersComponent
   ],
   imports: [
     BrowserModule,
